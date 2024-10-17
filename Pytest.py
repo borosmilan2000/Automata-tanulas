172acd0498c356bf5387eef3cb1a235a222c9f92
Milan Boros <milan.boros@lightware.com>
2024-10-17 15:45:42 +0200

    changes

--- Pytest.py
+++ Pytest.py
@@ -1,8 +1,11 @@
-import pytest
-
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
+def subtract(a, b):
+    return a + b  # <--- fix this in step 7
+
+def multiply(a, b):
+    return a * b
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
